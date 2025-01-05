3c76976e8421557ed67e6dcffafd715e512b91aa
Alvaro Beruvides <91335324+Alvaro1406@users.noreply.github.com>
2025-01-04 21:31:23 -0500

    Fix: In card of product and primary color.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,34 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
-  content: ['./components/**/*.{js,vue,ts}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}', './app.vue'],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
     extend: {
       colors: {
         primary: {
-          light: '#AE7DDD',
-          DEFAULT: process.env.PRIMARY_COLOR || '#7F54B2',
-          dark: '#754fa3',
+          light: "#AE7DDD",
+          DEFAULT: process.env.PRIMARY_COLOR || "#FF7A01",
+          dark: "#754fa3",
         },
       },
       aspectRatio: {
-        '9/8': '1 / 1.125',
+        "9/8": "1 / 1.125",
       },
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require("@tailwindcss/typography")],
 };
