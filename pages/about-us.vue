6b84340d41cfef6a1f79f413581047e66ba9534b
raydev9206 <raydel9206@gmail.com>
2025-02-05 15:32:25 -0500

    fix: order

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!loading" class="flex flex-col mt-7 items-center justify-center min-h-screen">
-    <h1 class="mb-8 text-3xl font-semibold text-primary" v-html="aboutus.data[0].title[locale]"></h1>
+    <h1 class="my-4 text-3xl font-semibold text-primary" v-html="aboutus.data[0].title[locale]"></h1>
     <div class="my-8 prose">
       <p v-html="aboutus.data[0].smalldescription[locale]"></p>
       <p v-html="aboutus.data[0].description[locale]"></p>
