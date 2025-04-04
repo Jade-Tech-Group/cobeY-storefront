8d1c7c5ce939d300227e3b85a5ffcdde57835588
raydev9206 <raydel9206@gmail.com>
2025-04-04 09:07:03 -0400

    fix: deploy

--- pages/paymentFailure.vue
+++ pages/paymentFailure.vue
@@ -1,7 +1,9 @@
 <template>
   <OrderErrorPayment status="error" message="Hubo un fallo en el pago" :orderid="id"/>
 </template>
-<script lang="ts">
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
 const route = useRoute();
 const id = route.params.id as string;
 </script>
