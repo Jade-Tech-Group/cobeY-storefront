8769d45b10bc15b602eeefe0289696ab4a023202
raydev9206 <raydel9206@gmail.com>
2025-04-17 23:42:29 -0400

    fix: toggle-cart

--- components/cartElements/CartTrigger.vue
+++ components/cartElements/CartTrigger.vue
@@ -8,7 +8,7 @@ const { toggleCart, cart } = useCart();
     title="Cart"
     @click="toggleCart"
   >
-    <Icon name="ion:cart" size="24" class="mr-1 mt-2 md:mr-0 text-white" />
+    <Icon name="ion:cart" size="24" class="mr-1 md:mr-0 text-white" />
     <ClientOnly>
       <Transition name="popIn" mode="out-in">
         <span
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,9 +1,22 @@
 <template>
-  <div v-if="!loading" class="flex flex-col mt-7 items-center justify-center min-h-screen">
-    <h1 class="my-4 text-3xl font-semibold text-primary" v-html="aboutus.data[0].title[locale]"></h1>
-    <div class="my-8 prose">
-      <p v-html="aboutus.data[0].smalldescription[locale]"></p>
-      <p v-html="aboutus.data[0].description[locale]"></p>
+  <div class="relative">
+    <div class="relative h-[250px] overflow-hidden"><img
+        src="https://raw.githubusercontent.com/RamiroSB/imagenes/main/banner-contactenos1.png"
+        alt="Product showcase"
+        class="absolute inset-0 object-cover w-full h-full transform"
+      />
+      <div class="absolute inset-0 bg-black bg-opacity-25"></div>
+      <div class="absolute inset-0 flex items-center justify-center">
+        <h1 class="text-4xl font-bold text-white text-center px-4">
+          {{ aboutus.data[0].title[locale] }}
+        </h1>
+      </div>
+    </div>
+    <div class="container flex items-start gap-16 relative -mt-16 bg-white rounded-t-3xl sm:p-8 xs:p-4 shadow-lg">
+      <div class="my-8 ">
+        <p v-html="aboutus.data[0].smalldescription[locale]"></p>
+        <p v-html="aboutus.data[0].description[locale]"></p>
+      </div>
     </div>
   </div>
 </template>
