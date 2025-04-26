832ddd2b3bd3df7388b5ed6b96d37953b8c66585
raydev9206 <raydel9206@gmail.com>
2025-04-25 23:38:59 -0400

    add:spot

--- pages/products.vue
+++ pages/products.vue
@@ -30,7 +30,19 @@ useHead({
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative overflow-hidden">
+    <div class="absolute inset-0 z-0 pointer-events-none">
+      <div
+        class="absolute sm:top-[25%] sm:-left-[25%] xs:-top-[10%] xs:-left-[25%] sm:w-[950px] sm:h-[950px] xs:w-[320px] xs:h-[320px] bg-[radial-gradient(circle_at_center,_rgba(239,122,20,0.3)_0%,_rgba(239,122,20,0.25)_15%,_rgba(239,122,20,0.2)_30%,_rgba(239,122,20,0.15)_45%,_rgba(255,255,255,0.1)_60%,_rgba(255,255,255,0.05)_75%,_rgba(255,255,255,0.02)_90%,_rgba(255,255,255,0)_100%)] rounded-full"
+      ></div>
+      <div
+        class="absolute sm:-bottom-[10%] -bottom-56 sm:right-[20%] xs:right-[0] sm:w-[750px] sm:h-[750px] xs:w-[320px] xs:h-[320px] bg-[radial-gradient(circle_at_center,_rgba(239,122,20,0.3)_0%,_rgba(239,122,20,0.25)_15%,_rgba(239,122,20,0.2)_30%,_rgba(239,122,20,0.15)_45%,_rgba(255,255,255,0.1)_60%,_rgba(255,255,255,0.05)_75%,_rgba(255,255,255,0.02)_90%,_rgba(255,255,255,0)_100%)] rounded-full"
+      ></div>
+      <div
+        v-if="!isMobile() || !isTablet()"
+        class="absolute -top-[10%] -right-[15%] sm:w-[950px] sm:h-[950px] xs:w-[320px] xs:h-[320px] bg-[radial-gradient(circle_at_center,_rgba(239,122,20,0.3)_0%,_rgba(239,122,20,0.25)_15%,_rgba(239,122,20,0.2)_30%,_rgba(239,122,20,0.15)_45%,_rgba(255,255,255,0.1)_60%,_rgba(255,255,255,0.05)_75%,_rgba(255,255,255,0.02)_90%,_rgba(255,255,255,0)_100%)] rounded-full"
+      ></div>
+    </div>
     <!-- Header with concave image -->
     <div class="relative h-[250px] overflow-hidden">
       <img
@@ -52,7 +64,9 @@ useHead({
     >
       <Filters v-if="storeSettings.showFilters" />
       <div class="w-full">
-        <div class="flex items-center justify-between w-full gap-4 md:gap-8 xs:px-2 sm:px-0">
+        <div
+          class="flex items-center justify-between w-full gap-4 md:gap-8 xs:px-2 sm:px-0"
+        >
           <ProductResultCount />
           <OrderByDropdown
             class="hidden md:inline-flex"
