b5b0a7ab793b98fb365d010f85436162397ad7ad
Raydel Herrera Garcia <raydel@gmail.com>
2025-07-09 13:46:31 -0400

    fix: deploy

--- conf/useConf.ts
+++ conf/useConf.ts
@@ -1,6 +1,6 @@
 export default {
   api: {
-    baseUrl: "https://cobey-api-d3vz.onrender.com/api/v1",
+    baseUrl: "https://cobey-api-ln81.onrender.com/api/v1",
     services: {
       auth: {
         login: "/auth/login",
